d5895372bf1230d66b48c897db1b57a8b3592478
Katherine Tam <katherinemtam@gmail.com>
2021-06-14 22:00:57 -0700

    / using GET route, test pass

--- __tests__/app.test.js
+++ __tests__/app.test.js
@@ -2,5 +2,21 @@ const request = require('supertest');
 const app = require('../lib/app');
 
 describe('app routes', () => {
+  test('get all from / ', async() => {
 
+    const res = await request(app)
+      .get('/');
+
+    expect(res.text).toEqual('hi'
+    );
+  });
+  
+  test('get all from / ', async() => {
+
+    const res = await request(app)
+      .get('/');
+
+    expect(res.text).toEqual('hi'
+    );
+  });
 });
